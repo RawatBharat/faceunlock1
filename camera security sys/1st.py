1342e6927cb456a91e2faf066a1ce9bed5539367
RawatBharat <88593849+RawatBharat@users.noreply.github.com>
2021-08-12 18:15:52 +0530

    Add files via upload

--- camera security sys/1st.py
+++ camera security sys/1st.py
@@ -1,30 +1,32 @@
-import numpy as np
-import cv2 
-face_cascade = cv2.CascadeClassifier('Haarcascade/haarcascade_frontalface_default.xml.txt')
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    
-    ret,frame=cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)   
-    for (x,y,w,h) in faces:
-       # print(x,y,w,h)
-        roi_gray = gray[y:y+h, x:x+w]
-        img_item = "my-image.png"
-        cv2.imwrite(img_item,roi_gray)
-        
-        color=(255,0,0)#BGR
-        stroke = 2
-        end_cord_x = x+w
-        end_cord_y = y+h
-        cv2.rectangle(frame, (x,y),(end_cord_x, end_cord_y), color, stroke)
-        
-    
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+'''s="lets reverse this string"
+for i in range(len(s)-1,-1,-1):
+    c=s[i]
+    print(c,"")
+'''
+'''
+s1="this is first string"
+s2="This is second string"
+s3="thisis. thir. dstring"
+s4="this is fourth string"
+print(s1.count("first"))
+print(s1.find("is"))
+print(s1.index("is"))
+print(s1.upper())
+print(s2.lower())
+print(s3.isalpha())
+print(s4.isdigit())
+s5=s2.replace("i","j")
+print(s5)
+print(s3.split('.'))    
+'''
+a="0o1234"
+b=int(a,base=8)
+print(b)
+c=oct(b)
+d=hex(b)
+e=int(b)
+print(c)
+print(d)
+print(e)
+print(int(0o123))
+print(int(b))
